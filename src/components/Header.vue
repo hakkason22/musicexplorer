6592ba94638338dec2ab28afe79391972e4eca5f
tairyu saito <tairyu@tairyunoMacBook-Pro.local>
2022-05-16 00:29:30 +0900

    ヘッダー&検索ワードバインディング

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="header_container">
+    <div class="header_logo_wrapper">
+        <h2>MusicExplorer</h2>
+    </div>
+    <div class="menu_wrapper">
+        <input type="text" v-model="artist_name" placeholder="artist name">
+        <input type="submit" value="go" @click="onClick">
+    </div>
+    
+</div>
+</template>
+<style>
+    body{
+        margin:0;
+        padding:0;
+    }
+    .header_container{
+        display: flex;
+        justify-content: space-between;
+        background: blue;
+        padding:0 40px;
+    }
+    .header_logo_wrapper{
+        color: white;
+    }
+    .menu_wrapper{
+        display: flex;
+        align-items: center;
+    }
+    
+</style>
+
+<script lang="ts">
+    import Vue from "vue";
+
+    export default Vue.extend({
+        data(){
+            return {
+                artist_name: ""
+            };
+        },
+        methods: {
+            created(){
+                console.log("test");
+            },
+            onClick(){
+                this.$emit('artist_name', this.artist_name);
+            }
+        }
+    })
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <h1>test</h1>
+  <Main>
+    <Header @artist_name='target_artist_name = $event' />
+    <h1>{{ target_artist_name }}</h1>
+    <h1 v-if="target_artist_name != ''">テストだよーーーーーん</h1>
+  </Main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  data(){
+    return {
+      target_artist_name: ""
+    };
+  }
 })
 </script>

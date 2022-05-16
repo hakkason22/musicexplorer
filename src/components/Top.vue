6916100c90af6748fe3b4b5f0c8058f6beed9545
tairyu saito <tairyu@tairyunoMacBook-Pro.local>
2022-05-16 21:47:45 +0900

    トップ画面

---
+++ src/components/Top.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="top_wrapper">
+        <div class="message_wrapper">
+            <h1>welcome to MusicExplorer, </h1>
+            <p>search your favrite artists.</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .top_wrapper{
+        text-align: center;
+        margin-top: 200px;
+    }
+</style>

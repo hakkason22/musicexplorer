3fa09acafed12a83ce618e301ab519d14f5357d2
tairyu saito <tairyu@tairyunoMacBook-Pro.local>
2022-05-18 17:11:40 +0900

    テスト用データ

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -32,50 +32,67 @@ export default Vue.extend({
       // });
 
       // test用
-      this.target_musics = [
-        {
-          music_name: 'lemon',
-          valence: 0.2,
-          arousal: 0.5,
-          music_url: "https//...1."
-        },
-        {
-          music_name: 'LOSER',
-          valence: 0.4,
-          arousal: 0.1,
-          music_url: "https//....2"
-        },
-        {
-          music_name: '曲３',
-          valence: 0.9,
-          arousal: 0.1,
-          music_url: "https//....3"
-        },
-        {
-          music_name: '曲４',
-          valence: 0,
-          arousal: 0,
-          music_url: "https//....4"
-        },
-        {
-          music_name: '曲５',
-          valence: 1,
-          arousal: 1,
-          music_url: "https//....5"
-        },
-        {
-          music_name: '曲６',
-          valence: 1,
-          arousal: 0,
-          music_url: "https//....6"
-        },
-        {
-          music_name: '曲７',
-          valence: 0,
-          arousal: 1,
-          music_url: "https//....7"
-        },
-      ];
+      if(this.target_artist_name !== "test"){
+          this.target_musics = [
+          {
+            music_name: 'lemon',
+            valence: 0.2,
+            arousal: 0.5,
+            music_url: "https//...1."
+          },
+          {
+            music_name: 'LOSER',
+            valence: 0.4,
+            arousal: 0.1,
+            music_url: "https//....2"
+          },
+          {
+            music_name: this.target_artist_name,
+            valence: 0.9,
+            arousal: 0.1,
+            music_url: "https//....3"
+          },
+          {
+            music_name: '曲４',
+            valence: 0,
+            arousal: 0,
+            music_url: "https//....4"
+          },
+          {
+            music_name: '曲５',
+            valence: 1,
+            arousal: 1,
+            music_url: "https//....5"
+          },
+          {
+            music_name: '曲６',
+            valence: 1,
+            arousal: 0,
+            music_url: "https//....6"
+          },
+          {
+            music_name: '曲７',
+            valence: 0,
+            arousal: 1,
+            music_url: "https//....7"
+          },
+        ];
+      }else{
+        this.target_musics = [
+          {
+            music_name: 'lemon',
+            valence: 0.6,
+            arousal: 0.2,
+            music_url: "https//...1."
+          },
+          {
+            music_name: 'LOSER',
+            valence: 0.7,
+            arousal: 0.4,
+            music_url: "https//....2"
+          },]
+      }
+      
     }
   },
 })

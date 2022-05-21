f1c3350e4e6b4ca66ed4dfddb4ba9b88c74b63b0
tairyu saito <tairyu@tairyunoMacBook-Pro.local>
2022-05-21 23:57:22 +0900

    お気に入り機能２

--- src/components/FavariteModal.vue
+++ src/components/FavariteModal.vue
@@ -8,7 +8,7 @@
                 <h2>グラフにお気に入り曲を追加</h2>
                 <div class="favorite_infos">
                     <div class="favorite_info" v-for="favoriteMusicInfo in favoriteMusicInfos" :key="favoriteMusicInfo.music_id">
-                        <input type="radio" :id="favoriteMusicInfo.music_id" :value="favoriteMusicInfo.music_id" v-model="selected_music_id">
+                        <input type="radio" :id="favoriteMusicInfo.music_id" :value="favoriteMusicInfo.music_id" v-model="selectedMusicId">
                         {{ favoriteMusicInfo.music_name }}
                     </div>
                 </div>
@@ -22,15 +22,28 @@
 <script lang="ts">
 import Vue from 'vue'
 import axios from 'axios'
+
+type musicInfos = {
+    music_id: string,
+    music_name: string,
+    valence: number,
+    energy: number,
+
+}
+
+export type datatype = {
+    favoriteMusicInfos: musicInfos[],
+    selectedMusicId: string
+}
+
 export default Vue.extend({
-    data(){
+    data(): datatype{
         return {
             favoriteMusicInfos: [],
-            selected_music_id: "",
+            selectedMusicId: "",
         }
     },
     mounted(){
-        this.testVal = "mounted!!!"
         this.getFavoriteMusicInfo()
     },
     methods:{
@@ -38,80 +51,80 @@ export default Vue.extend({
             this.$store.commit('closeFavoriteModal');
         },
         getFavoriteMusicInfo(){
-            // const user_id = this.$store.getters.user.uid
-            // const url = "http://52.192.42.106/music/favorite/list"
-            // const params = new URLSearchParams()
-            // params.append('user_id', user_id)
-            // axios.post(url, params).then((response) => {
-            //     this.favoriteMusicInfos= response.data
-            //     console.log(response.data)
-            // })
+            const user_id: string = this.$store.getters.user.uid
+            const url: string = "http://52.192.42.106/music/favorite/list"
+            const params: any = new URLSearchParams()
+            params.append('user_id', user_id)
+            axios.post(url, params).then((response) => {
+                this.favoriteMusicInfos= response.data
+                console.log(response.data)
+            })
 
-            this.favoriteMusicInfos = [
-          {
-            music_name: 'ミラーチューン',
-            valence: 0.895,
-            energy: 0.933,
-            music_id: "0R8JLNP107Hr7V7lL9oh13"
-          },
-          {
-            music_name: 'あいつら全員同窓会',
-            valence: 0.913,
-            energy: 0.913,
-            music_id: "2VIK6jaaKghS4QPHr6sAkv"
-          },
-          {
-            music_name: "秒針を噛む",
-            valence: 0.775,
-            energy: 0.962,
-            music_id: "4HgsPAX3MmMgIT60hJ4W4U"
-          },
-          {
-            music_name: '正しくなれない',
-            valence: 0.484,
-            energy: 0.679,
-            music_id: "2FHexlZenTsS3lsjUYHiDx"
-          },
-          {
-            music_name: '勘冴えて悔しいわ',
-            valence: 0.762,
-            energy: 0.924,
-            music_id: "7zbfS30vKiHU8oBs6Wi1Qp"
-          },
-          {
-            music_name: '袖のキルト',
-            valence: 0.658,
-            energy: 0.813,
-            music_id: "7iDC1sNnfChQhMmdBuZmBA"
-          },
-          {
-            music_name: 'お勉強しといてよ',
-            valence: 0.868,
-            energy: 0.875,
-            music_id: "3sgJTMHqDwxfsBIMqvkXKE"
-          },
-          {
-            music_name: 'ばかじゃないのに',
-            valence: 0.739,
-            energy: 0.937,
-            music_id: "2UkcZV07LP39NQg9tGBvnh"
-          },
-          {
-            music_name: '猫リセット',
-            valence: 0.935,
-            energy: 0.902,
-            music_id: "68TwxeHi1pyFDRIKHxSArM"
-          },
-          {
-            music_name: '違う曲にしようよ',
-            valence: 0.9,
-            energy: 0.806,
-            music_id: "7abFHUH53rYlZwmsjZCvND"
-          },
-        ];
+        //     this.favoriteMusicInfos = [
+        //   {
+        //     music_name: 'ミラーチューン',
+        //     valence: 0.895,
+        //     energy: 0.933,
+        //     music_id: "0R8JLNP107Hr7V7lL9oh13"
+        //   },
+        //   {
+        //     music_name: 'あいつら全員同窓会',
+        //     valence: 0.913,
+        //     energy: 0.913,
+        //     music_id: "2VIK6jaaKghS4QPHr6sAkv"
+        //   },
+        //   {
+        //     music_name: "秒針を噛む",
+        //     valence: 0.775,
+        //     energy: 0.962,
+        //     music_id: "4HgsPAX3MmMgIT60hJ4W4U"
+        //   },
+        //   {
+        //     music_name: '正しくなれない',
+        //     valence: 0.484,
+        //     energy: 0.679,
+        //     music_id: "2FHexlZenTsS3lsjUYHiDx"
+        //   },
+        //   {
+        //     music_name: '勘冴えて悔しいわ',
+        //     valence: 0.762,
+        //     energy: 0.924,
+        //     music_id: "7zbfS30vKiHU8oBs6Wi1Qp"
+        //   },
+        //   {
+        //     music_name: '袖のキルト',
+        //     valence: 0.658,
+        //     energy: 0.813,
+        //     music_id: "7iDC1sNnfChQhMmdBuZmBA"
+        //   },
+        //   {
+        //     music_name: 'お勉強しといてよ',
+        //     valence: 0.868,
+        //     energy: 0.875,
+        //     music_id: "3sgJTMHqDwxfsBIMqvkXKE"
+        //   },
+        //   {
+        //     music_name: 'ばかじゃないのに',
+        //     valence: 0.739,
+        //     energy: 0.937,
+        //     music_id: "2UkcZV07LP39NQg9tGBvnh"
+        //   },
+        //   {
+        //     music_name: '猫リセット',
+        //     valence: 0.935,
+        //     energy: 0.902,
+        //     music_id: "68TwxeHi1pyFDRIKHxSArM"
+        //   },
+        //   {
+        //     music_name: '違う曲にしようよ',
+        //     valence: 0.9,
+        //     energy: 0.806,
+        //     music_id: "7abFHUH53rYlZwmsjZCvND"
+        //   },
+        // ];
         },
         addChart(){
-            const targetMusicInfo = this.favoriteMusicInfos.find(element => element.music_id === this.selected_music_id)
+            const targetMusicInfo = this.favoriteMusicInfos.find(element => element.music_id === this.selectedMusicId)
             this.$nuxt.$emit('addFavoriteToChart', targetMusicInfo)
             this.closeFavoriteModal()
         }

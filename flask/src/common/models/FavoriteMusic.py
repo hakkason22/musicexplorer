670c96751498038952de2992ef19c04715d7392f
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-02 21:47:20 +0900

    add:Base.py:モデルクラスの基底クラスwo

--- flask/src/common/models/FavoriteMusic.py
+++ flask/src/common/models/FavoriteMusic.py
@@ -1,12 +1,31 @@
-class FavoriteMusic:
+from datetime import datetime
+from common.models.Base import Base
+
+class FavoriteMusic(Base):
     def __init__(self) -> None:
-        pass
-
-    def get_music_list():
-        pass
-
-    def register():
-        pass
-
-    def delete():
-        pass
+        super().__init__()
+        self.id = self.Column(
+            self.Mediumint(unsigned=True), 
+            primary_key=True,
+            autoincrement=True)
+        self.music_id = self.Column(
+            self.String(255), 
+            nullable=False,
+            unique=True)
+        self.music_name = self.Column(
+            self.String(255), 
+            nullable=False)
+        self.valence = self.Column(
+            self.Float(8), 
+            nullable=False)
+        self.energy = self.Column(
+            self.Float(8), 
+            nullable=False)
+        self.user_id = self.Column(
+            self.String(255), 
+            nullable=False)
+        self.created_at = self.Column(
+            self.DateTime, 
+            nullable=False, 
+            default=datetime.now, 
+            onupdate=datetime.now)

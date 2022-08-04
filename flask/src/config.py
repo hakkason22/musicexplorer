1c07572e816bf5b6f9e6a23b90b6332ecf026c64
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-04 17:37:42 +0900

    fix:データベースのホスト名を修正

--- flask/src/config.py
+++ flask/src/config.py
@@ -10,8 +10,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/musicexplorer?charset=utf8'.format(**{
         'user': os.getenv('DB_USER_NAME', 'root'),
         'password': os.getenv('DB_USER_PASS', ''),
-        'host': 'localhost',
-        # 'host': os.getenv('DB_HOST_NAME', 'localhost'),
+        # 'host': 'localhost',
+        'host': os.getenv('DB_HOST_NAME', 'localhost'),
     })
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False

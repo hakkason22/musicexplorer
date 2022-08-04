bb6606c1c436feae1d13a3b7f7f77357bbd05c02
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-04 17:46:26 +0900

    add:お気に入り楽曲の情報の内クライアントに渡すものを定義

---
+++ flask/src/common/libs/FavoriteMusicData.py
@@ -0,0 +1,9 @@
+
+from common.models.FavoriteMusic import FavoriteMusic
+
+class FavoriteMusicData:
+    def __init__(self,favorite_music:FavoriteMusic) -> None:
+            self.id = favorite_music.id
+            self.music_name = favorite_music.music_name
+            self.valence = favorite_music.valence
+            self.energy = favorite_music.energy

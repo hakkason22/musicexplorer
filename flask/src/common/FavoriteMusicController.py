a56b638ccfe0d5bdf5b19212f9c9d9d77eff4a84
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-05 13:37:28 +0900

    fix:エラーの返却処理を修正

--- flask/src/common/FavoriteMusicController.py
+++ flask/src/common/FavoriteMusicController.py
@@ -12,29 +12,29 @@ class FavoriteMusicController:
         self.__request_data = favorite_music_data
 
     def run(self):
-
         if self.__purpose == 'register':
-            self.register()
-            return {'message':'success!!'}
+            return self.register()
         elif self.__purpose == 'list':
             return self.get_favorite_musics()
         elif self.__purpose == 'delete':
-            self.delete()
-            return {'message':'success!!'}
+            return self.delete()
         else:
-            return {'message':'false!!'}
+            return {"error_message":"Invalid request","result":0}
 
     def register(self):
         """お気に入り登録
         """
-        # 登録するお気に入り曲オブジェクトを作成
-        register_favorite_music = self.__FavoriteMusic(**self.__request_data)
+        try:
+            # 登録するお気に入り曲オブジェクトを作成
+            register_favorite_music = self.__FavoriteMusic(**self.__request_data)
 
-        # お気に入り曲を登録
-        self.__db.session.add(register_favorite_music)
-        self.__db.session.commit()
+            # お気に入り曲を登録
+            self.__db.session.add(register_favorite_music)
+            self.__db.session.commit()
+        except Exception as e:
+            return {"error_message":e,"result":0}
         
-        return
+        return {"error_message":"","result":1}
 
     def get_favorite_musics(self):
         """お気に入り全取得
@@ -42,24 +42,31 @@ class FavoriteMusicController:
         Returns:
             list: [{key1:value,key2:value…},{…},…]
         """
-        # ユーザーidからお気に入り曲を全取得
-        find_favorite_musics = self.__FavoriteMusic.query.filter_by(user_id = self.__request_data['user_id']).all()
+        try:
+            # ユーザーidからお気に入り曲を全取得
+            find_favorite_musics = self.__FavoriteMusic.query.filter_by(user_id = self.__request_data['user_id']).all()
 
-        # お気に入り曲オブジェクトを FavoriteMusicData でラップ（クライアントに渡す値を定義）して、そのリストを戻り値にする
-        find_favorite_musics_re = []
-        for m in find_favorite_musics:
-            find_favorite_musics_re.append(vars(FavoriteMusicData(m)))
+            # お気に入り曲オブジェクトを FavoriteMusicData でラップ（クライアントに渡す値を定義）して、そのリストを戻り値にする
+            find_favorite_musics_re = []
+            for m in find_favorite_musics:
+                find_favorite_musics_re.append(vars(FavoriteMusicData(m)))
+        except Exception as e:
+            return {"error_message":e,"result":0}
 
-        return find_favorite_musics_re
+        re = {"error_message":"","result":1}
+        re['data'] = find_favorite_musics_re
+        return re
 
     def delete(self):
         """お気に入り削除
         """
         # レコードのid　からお気に入り曲を削除
-        for id in self.__request_data['favorite_music_ids']:
-            delete_favorite_music = self.__FavoriteMusic.query.get(int(id))
-            self.__db.session.delete(delete_favorite_music)
+        try:
+            for id in self.__request_data['favorite_music_ids']:
+                delete_favorite_music = self.__FavoriteMusic.query.get(int(id))
+                self.__db.session.delete(delete_favorite_music)
+            self.__db.session.commit()
+        except Exception as e:
+            return {"error_message":e,"result":0}
 
-        self.__db.session.commit()
-        
-        return
+        return {"error_message":"","result":1}

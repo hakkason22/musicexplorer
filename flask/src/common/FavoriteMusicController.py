79b6f4ff54a5483055e0d01e1c2eb05c31034878
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-05 13:55:25 +0900

    fix:コメントアウトを修正

--- flask/src/common/FavoriteMusicController.py
+++ flask/src/common/FavoriteMusicController.py
@@ -5,6 +5,8 @@ import os
 from common.libs.FavoriteMusicData import FavoriteMusicData
 
 class FavoriteMusicController:
+    """お気に入り楽曲を管理するコントローラ
+    """
     def __init__(self,db,model,purpose:str,favorite_music_data:dict):
         self.__db = db
         self.__FavoriteMusic = model
@@ -12,6 +14,11 @@ class FavoriteMusicController:
         self.__request_data = favorite_music_data
 
     def run(self):
+        """状態によって処理を振り分ける
+
+        Returns:
+            dict: { error_message:(str), result:(0or1)[, data:(list)] }
+        """
         if self.__purpose == 'register':
             return self.register()
         elif self.__purpose == 'list':
@@ -23,6 +30,9 @@ class FavoriteMusicController:
 
     def register(self):
         """お気に入り登録
+
+        Returns:
+            dict: { error_message:(str), result:(0or1) }
         """
         try:
             # 登録するお気に入り曲オブジェクトを作成
@@ -32,15 +42,15 @@ class FavoriteMusicController:
             self.__db.session.add(register_favorite_music)
             self.__db.session.commit()
         except Exception as e:
-            return {"error_message":e,"result":0}
+            return {"error_message":e, "result":0}
         
-        return {"error_message":"","result":1}
+        return {"error_message":"", "result":1}
 
     def get_favorite_musics(self):
         """お気に入り全取得
 
         Returns:
-            list: [{key1:value,key2:value…},{…},…]
+            dict: { error_message:(str), result:(0or1), data:(list) }
         """
         try:
             # ユーザーidからお気に入り曲を全取得
@@ -59,6 +69,9 @@ class FavoriteMusicController:
 
     def delete(self):
         """お気に入り削除
+
+        Returns:
+            dict: { error_message:(str), result:(0or1) }
         """
         # レコードのid　からお気に入り曲を削除
         try:

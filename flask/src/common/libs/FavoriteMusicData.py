cf0a1c9ca5ce7fee8595ae69df6b77ad0161c01c
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-10 10:50:54 +0900

    fix:タイポ修正

--- flask/src/common/libs/FavoriteMusicData.py
+++ flask/src/common/libs/FavoriteMusicData.py
@@ -4,7 +4,7 @@ from common.models.FavoriteMusic import FavoriteMusic
 class FavoriteMusicData:
     def __init__(self,favorite_music:FavoriteMusic) -> None:
             self.favorite_music_id = favorite_music.id
-            self.music_name = favorite_music.music_id
+            self.music_id = favorite_music.music_id
             self.music_name = favorite_music.music_name
             self.valence = favorite_music.valence
             self.energy = favorite_music.energy

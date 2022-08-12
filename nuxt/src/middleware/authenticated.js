0a7ce539c94060161a07b425a5e974b3eac3e724
namikis <koudoldk@icloud.com>
2022-08-13 03:04:04 +0900

    未ログインでもお気に入り以外は使えるように変更

--- nuxt/src/middleware/authenticated.js
+++ nuxt/src/middleware/authenticated.js
@@ -1,5 +1,5 @@
 export default ({ store, route, redirect }) => {
-    if(!store.getters.isAuthenticated && route.name !== 'login' && route.name !== 'register'){
+    if(!store.getters.isAuthenticated && route.name !== 'login' && route.name !== 'register' && route.path === '/manage_favorite'){
         redirect('/login')
     }
     if(store.getters.isAuthenticated && (route.name === 'login' || route.name === 'register')){

5070236343ae38894ef404e51e8c41275fe61cae
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-13 23:16:35 +0900

    add:コメントアウトした

--- flask/src/common/libs/FavoriteMusicService.py
+++ flask/src/common/libs/FavoriteMusicService.py
@@ -5,6 +5,15 @@ class FavoriteMusicService:
         pass
 
     def CheckExist(user_id:str,music_id:str):
+        """ユーザーのお気に入り曲の重複確認
+
+        Args:
+            user_id (str): ユーザーID
+            music_id (str): 楽曲のID
+
+        Returns:
+            bool: 存在していたらTrue、存在していなかったらFalse
+        """
         result = FavoriteMusic.query.filter_by(user_id = user_id,music_id = music_id).first()
         if not result:
             return False

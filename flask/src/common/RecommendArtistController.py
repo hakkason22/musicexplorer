752da9253a9450c3669d563a3c144ea06231b0e9
k20098kk <k20098kk@k20098kknoMacBook-Air-5.local>
2022-08-22 10:50:46 +0900

    fix:ゲストの時は、人気アーティストを返す用にした

--- flask/src/common/RecommendArtistController.py
+++ flask/src/common/RecommendArtistController.py
@@ -7,6 +7,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
 import random
+import datetime
 
 class RecommendArtistController:
     """おすすめアーティストに関するコントローラ
@@ -18,12 +19,21 @@ class RecommendArtistController:
             language='ja')
 
     def get_recommend_artists(self,user_id):
+        # user_id が空だったら、有名アーティストを返す
+        if not user_id:
+            return self.get_popular_artists()
+
         try:
             # ユーザーidからお気に入り曲を全取得
+            # 全取得しないようにしたい
             favorite_musics = FavoriteMusic.query.filter_by(user_id = user_id).all()
         except Exception as e:
             print(str(e))
             return {"error_message": str(e),"result":0}
+
+        # お気に入り曲が空だったら、有名アーティストを返す
+        if not favorite_musics:
+            return self.get_popular_artists()
 
         # お気に入り曲からランダムな一つを取得
         rand_index = random.randint(0,len(favorite_musics)-1)
@@ -39,6 +49,26 @@ class RecommendArtistController:
         recommend_artists_re = []
         for artist in recommend_artists['artists']:
             # Artist オブジェクトを生成して、RecommendArtistsData に　値を移し替える、vars() でフィールドを dict 型で取得
+            recommend_artists_re.append(vars(RecommendArtistData(Artist(artist['id'],artist['name'],artist['images']))))
+        return recommend_artists_re
+
+    def get_popular_artists(self):
+        """人気アーティストを返す
+        """
+        currentDateTime = datetime.datetime.now()
+        date = currentDateTime.date()
+        year = date.strftime("%Y")
+
+        # 現在の年で検索
+        # マーケットはとりあえずJPにした
+        recommend_artists = self.spotify.search(q=f"year:{year}",limit=20,type='artist',market='JP')
+
+        recommend_artists_re = []
+        for artist in recommend_artists['artists']['items']:
+            # Artist オブジェクトを生成して、RecommendArtistsData に　値を移し替える、vars() でフィールドを dict 型で取得
+            # 人気度が50未満のアーティストはスルー
+            if artist['popularity'] < 50:
+                continue
             recommend_artists_re.append(vars(RecommendArtistData(Artist(artist['id'],artist['name'],artist['images']))))
         return recommend_artists_re
 
